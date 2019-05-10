fffaecc78e1ac0d5384e002721a41b7e02b4eddf
seunga <_970813@daum.net>
2019-05-09 20:58:43 -0700

    Create files and change variables
    - InitGame.h, InitGame.c 생성: main 함수를 분리하여 함수화 기본 마련
    - 변수 수정: main 함수의 변수를 의미 있는 이름으로 개선

---
+++ InitGame.h
@@ -0,0 +1,26 @@
+#pragma once
+
+char key;
+
+void setUp();
+void printBoard();
+void findEmpty();
+
+void inputKey();
+void refreshGame();
+void undo();
+
+void moveRight();
+void moveLeft();
+void moveUp();
+void moveDown();
+
+void pushRight();
+void pushLeft();
+void pushUp();
+void pushDown();
+
+void mergeRight();
+void mergeLeft();
+void mergeUp();
+void mergeDown();
