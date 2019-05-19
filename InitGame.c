a47deb5cf1d474a9f04bb35b6057ef1f9e4dc9c4
wlals7217 <48884808+wlals7217@users.noreply.github.com>
2019-05-19 23:59:23 +0900

    make function (move, push, merge, checkMove)
    -move함수내부의 스코어계산관련함수 제거.
    -push 함수, merge함수 메인에서 분리.
    -move함수 벡터쓰면 과하게 복잡한 함수가 될 것. 부속함수에서 분기 하도록
    수정.
    -연산자 좌우 띄어쓰기 수정.

--- InitGame.c
+++ InitGame.c
@@ -1,33 +1,35 @@
+#include "pch.h"
+
 #include "InitGame.h"
 #include <stdlib.h>
 #include <string.h>
 
 int** allocateArr(int **arr, int size) {
-	int i=0, j=0;
+	int i = 0, j = 0;
 
-	arr=(int **)malloc(sizeof(int *)*size);
-	for (i=0; i<size; i++) {
-		arr[i]=(int *)malloc(sizeof(int)*size);
+	arr = (int **)malloc(sizeof(int *)*size);
+	for (i = 0; i < size; i++) {
+		arr[i] = (int *)malloc(sizeof(int)*size);
 	}
-	for (i=0; i<size; i++) {
-		for (j=0; j<size; j++) {
-			arr[i][j]=0;
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < size; j++) {
+			arr[i][j] = 0;
 		}
 	}
 	return arr;
 }//arr에 size만큼 메모리 할당하고 0으로 초기화
 
 int** setUp(int **arr, int size) {
-	int k=0, i=0, j=0;
-	int random=0;
+	int k = 0, i = 0, j = 0;
+	int random = 0;
 
-	key=0;
-	arr=allocateArr(arr, size);
-	for (k=0; k<2; k++) {
-		i=rand()%size;
-		j=rand()%size;
-		random=((rand()%2)+1)*2;
-		arr[i][j]=random;
+	key = 0;
+	arr = allocateArr(arr, size);
+	for (k = 0; k < 2; k++) {
+		i = rand() % size;
+		j = rand() % size;
+		random = ((rand() % 2) + 1) * 2;
+		arr[i][j] = random;
 	}
 	return arr;
 }//메모리 할당된 arr에 랜덤 수 2개 생성
@@ -40,52 +42,209 @@ void refreshGame() {}
 void undo() {}
 
 
-void save(int **cur_board, int **pre_board, int *cur_scroe, int *pre_score);
-void push(int **cur_board, int vecx, int vecy);
-void merge(int **cur_board, int vecx, int vecy);
-bool checkMove(int **cur_board, int **pre_board);
-void printBoard(int **cur_board, int **pre_board, int *cur_score, int *pre_score);
+void save(int **cur_board, int **pre_board, int *cur_score, int *pre_score, int size) {
+	int i = 0;
+	int j = 0;
 
-int move(char key, int **cur_board, int **pre_board) {
-	
-	int vecx = 0;
-	int vecy = 0;
-	
-	/* 블럭이 움직이는 방향에따른 벡터값을 부여합니다. */
+	pre_score = cur_score;
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < size; j++) {
+			pre_board[i][j] = cur_board[i][j];
+		}
+	}
+}
+
+void push(int **cur_board, char key, int size) {
+	int k = 0;
+	int i = 0;
+	int j = 0;
 
 	switch (key) {
-	case w:
-		vecx = 	0;
-		vecy = -1;
-	break;
-	case a:
-		vecx = -1;
-		vecy = 	0;
-	break;
-	case s:
-		vecx = 	0;
-		vecy = 	1;
-	break;
-	case d:
-		vecx =	1;
-		vecy = 	0;
-	break;
-	}
-	
-	push(cur_board, vecx, vecy);
-	
-	merge(cur_board, vecx, vecy);
+	case 'w':
+	case 'W':
+		for (k = 0; k <= size - 2; k++) {
+			for (j = 0; j <= size - 1; j++) {
+				for (i = 0; i < size - 1; i++) {
+					if (cur_board[i][j] == 0 && cur_board[i + 1][j] != 0) {
+						cur_board[i][j] = cur_board[i + 1][j];
+						cur_board[i + 1][j] = 0;
+					}
+				}
+			}
+		}
 
-	if(checkMove(int **cur_board, int **pre_board)){ /* 블록이  전혀 움직이지 않은 경우 
-							    1을 반환하고 함수를 종료합니다.*/
-		return 1;
+		break;
+	case 'a':
+	case 'A':
+		for (k = 0; k <= size - 2; k++) {
+			for (i = 0; i <= size - 1; i++) {
+				for (j = size - 1; j > 0; j--) {
+					if (cur_board[i][j] != 0 && cur_board[i][j - 1] == 0) {
+						cur_board[i][j - 1] = cur_board[i][j];
+						cur_board[i][j] = 0;
+					}
+				}
+			}
+		}
+
+		break;
+	case 's':
+	case 'S':
+		for (k = 0; k <= size - 1; k++) {
+			for (j = 0; j <= size - 1; j++) {
+				for (i = size - 1; i > 0; i--) {
+					if (cur_board[i][j] == 0 && cur_board[i - 1][j] != 0) {
+						cur_board[i][j] = cur_board[i - 1][j];
+						cur_board[i - 1][j] = 0;
+					}
+				}
+			}
+		}
+		break;
+	case 'd':
+	case 'D':
+		for (k = 0; k <= size - 2; k++) {
+			for (i = 0; i <= size - 1; i++) {
+				for (j = 0; j < size - 1; j++) {
+					if (cur_board[i][j] != 0 && cur_board[i][j + 1] == 0) {
+						cur_board[i][j + 1] = cur_board[i][j];
+						cur_board[i][j] = 0;
+					}
+				}
+			}
+		}
+		break;
 	}
-	
-	push(cur_board, vecx, vecy);
+}
+
+void merge(int **cur_board, char key, int size) {
+
+	int i = 0;
+	int j = 0;
+	int flag = 0;
+
+	switch (key) {
+	case 'w':
+	case 'W':
+		for (j = 0; j <= size - 1; j++) {
+			i = 0;
+			flag = 0;
+			while (i < size - 1 && flag == 0) {
+
+				if (cur_board[i][j] == cur_board[i + 1][j] && cur_board[i][j] != 0) {
+					cur_board[i][j] = cur_board[i + 1][j] + cur_board[i][j];
+					cur_board[i + 1][j] = 0;
+					flag = 1;
+				}
+				i = i + 1;
+			}
+		}
+		break;
+	case 'a':
+	case 'A':
+		for (i = 0; i <= size - 1; i++) {
+			j = 0;
+			flag = 0;
+			while (j <= size - 2 && flag == 0) {
+				if (cur_board[i][j] == cur_board[i][j + 1] && cur_board[i][j] != 0) {
+					cur_board[i][j] = cur_board[i][j + 1] + cur_board[i][j];
+					cur_board[i][j + 1] = 0;
+					flag = 1;
+				}
+				j = j + 1;
+			}
+		}
+		break;
+	case 's':
+	case 'S':
+		for (j = 0; j <= size - 1; j++) {
+			i = size - 1;
+			flag = 0;
+			while (i > 0 && flag == 0) {
+				if (cur_board[i][j] == cur_board[i - 1][j] && cur_board[i][j] != 0) {
+					cur_board[i][j] = cur_board[i - 1][j] + cur_board[i][j];
+					cur_board[i - 1][j] = 0;
+					flag = 1;
+				}
+				i = i - 1;
+			}
+		}
+		break;
+	case 'd':
+	case 'D':
+		for (i = 0; i <= size - 1; i++) {
+			j = 0;
+			flag = 0;
+			while (j <= size - 2 && flag == 0) {
+				if (cur_board[i][j] == cur_board[i][j + 1] && cur_board[i][j] != 0) {
+					cur_board[i][j + 1] = cur_board[i][j + 1] + cur_board[i][j];
+					cur_board[i][j] = 0;
+					flag = 1;
+				}
+				j = j + 1;
+			}
+		}
+		break;
+	}
+}
+
+int checkMove(int **cur_board, int **pre_board, int size) {	/* 보드에 변경사항이 있는지 검사하고 변경사항이 있으면 조기리턴한다. */
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < size; j++) {
+			if (cur_board[i][j] != pre_board[i][j])
+				return 0;
+		}
+	}
+	return 1;
+};
+
+void printBoard(int **cur_board, int **pre_board, int *cur_score, int *pre_score) {}
+
+void spawnBlock(int **cur_board, int *emptyIndex, int size) {
+
+	int emptyTile = 0;
+	int randomIndex = 0;
+	int randomNum = 0;
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; i <= size - 1; i++) {
+		for (j = 0; j <= size; j++) {
+			if (cur_board[i][j] == 0) {
+				emptyIndex[emptyTile] = 10 * i + j;
+				emptyTile = emptyTile + 1;
+			}
+		}
+	}
+	randomIndex = rand() % emptyTile;
+	i = emptyIndex[randomIndex] / 10;
+	j = emptyIndex[randomIndex] % 10;
+	randomNum = ((rand() % 2) + 1) * 2;
+	cur_board[i][j] = randomNum;
 
 }
 
-void updateScore(int **cur_board, int *cur_score, int *high_score)
+int move(char key, int **cur_board, int **pre_board, int size) { /* 블록을 옮기고 붙어있는 블럭을 합치고 다시 한 번 블록을 옮긴다.
+									merge 이후 생기는 틈을 매꾼다.*/
+	push(cur_board, key, size);
+
+	merge(cur_board, key, size);
+
+	if (checkMove(cur_board, pre_board, size)) { /* 블록이  전혀 움직이지 않은 경우
+								1을 반환하고 함수를 종료한다. 움직이지 않았을때 다시 키를 받도록 한다. */
+		return 1;
+	}
+
+	push(cur_board, key, size);
+
+}
+
+void updateScore(int **cur_board, int *cur_score, int *high_score, int size) {
+
+}
 
 
 
--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <time.h>
 #include <conio.h>
@@ -35,29 +36,29 @@ int main() {
 	- int flag: 이동 후 인접 타일 더할 때 두 번 이상 반복해 더함 방지
 	*/
 
-	int **cur_board=NULL, **pre_board=NULL, *emptyIndex=NULL;
+	int **cur_board = NULL, **pre_board = NULL, *emptyIndex = NULL;
 	int size, cur_score, high_score, pre_score, sum;
-	int isGameover=0, isWin;
-	int emptyTile=1, randomNum, randomIndex=0;
-	int i, j, k, flag=0;
+	int isGameover = 0, isWin;
+	int emptyTile = 1, randomNum, randomIndex = 0;
+	int i, j, k, flag = 0;
 
 	printf("Enter the desired game board size: ");
-	scanf("%d", &size);
+	scanf_s("%d", &size);
 
-	cur_board=setUp(cur_board, size);//메모리 할당 후 값 0으로 초기화
-	pre_board=allocateArr(pre_board, size);//메모리 할당
-	emptyIndex=(int *)malloc(sizeof(int)*(size*size));
+	cur_board = setUp(cur_board, size);//메모리 할당 후 값 0으로 초기화
+	pre_board = allocateArr(pre_board, size);//메모리 할당
+	emptyIndex = (int *)malloc(sizeof(int)*(size*size));
 
-	srand(time(NULL));
+	srand((unsigned int)time(NULL));
 
-	pre_score=0;
-	cur_score=0;
-	high_score=0;
-	sum=0;
+	pre_score = 0;
+	cur_score = 0;
+	high_score = 0;
+	sum = 0;
 
-	for (i=0; i <size; i++) {
-		for (j=0; j<size; j++) {
-			if (j==0)
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < size; j++) {
+			if (j == 0)
 				printf("|");
 			printf(" %4d   |", cur_board[i][j]);
 		}
@@ -67,71 +68,39 @@ int main() {
 	printf("Score:%d\nHigh Score=%d", cur_score, high_score);
 	printf("\nPress w to move up,press a to move left,press s to move down,press d to move right,\npress e to finish the game, press x to refresh,press r to restore \n");
 
-	while (key!='e') {
-		key=_getch();
-		srand(time(NULL));
+	while (key != 'e') {
+		key = _getch();
+		srand((unsigned int)time(NULL));
 
-		if (key=='d') {
-			pre_score=cur_score;
-			for (i=0; i<size; i++) {
-				for (j=0; j<size; j++) {
-					pre_board[i][j]=cur_board[i][j];
+		if (key == 'd') {
+			pre_score = cur_score;
+			for (i = 0; i < size; i++) {
+				for (j = 0; j < size; j++) {
+					pre_board[i][j] = cur_board[i][j];
 				}
 			}
-			for (k=0; k<=size-2; k++) {
 
-				for (i=0; i<=size-1; i++) {
-					for (j=0; j < size-1; j++) {
-						if (cur_board[i][j]!=0&&cur_board[i][j+1]==0) {
-							cur_board[i][j+1]=cur_board[i][j];
-							cur_board[i][j]=0;
-						}
+			move(key, cur_board, pre_board, size);
+
+			emptyTile = 0;
+			for (i = 0; i <= size - 1; i++) {
+				for (j = 0; j <= size; j++) {
+					if (cur_board[i][j] == 0) {
+						emptyIndex[emptyTile] = 10 * i + j;
+						emptyTile = emptyTile + 1;
 					}
 				}
 			}
-			for (i=0; i<=size-1; i++) {
-				j=0;
-				flag=0;
-				while (j<=size-2 && flag==0) {
-
-					if (cur_board[i][j]==cur_board[i][j+1]&&cur_board[i][j]!=0) {
-						cur_board[i][j+1]=cur_board[i][j+1]+cur_board[i][j];
-						cur_board[i][j]=0;
-						flag=1;
-					}
-					j=j+1;
-				}
-			}
-			for (k=0; k<=size-2; k++) {
-
-				for (i=0; i<=size-1; i++) {
-					for (j=0; j < size-1; j++) {
-						if (cur_board[i][j]!=0&&cur_board[i][j+1]==0) {
-							cur_board[i][j+1]=cur_board[i][j];
-							cur_board[i][j]=0;
-						}
-					}
-				}
-			}
-			emptyTile=0;
-			for (i=0; i<=size-1; i++) {
-				for (j=0; j<=size; j++) {
-					if (cur_board[i][j]==0) {
-						emptyIndex[emptyTile]=10*i+j;
-						emptyTile=emptyTile+1;
-					}
-				}
-			}
-			randomIndex=rand()%emptyTile;
-			i=emptyIndex[randomIndex]/10;
-			j=emptyIndex[randomIndex]%10;
-			randomNum=((rand()%2)+1)*2;
-			cur_board[i][j]=randomNum;
+			randomIndex = rand() % emptyTile;
+			i = emptyIndex[randomIndex] / 10;
+			j = emptyIndex[randomIndex] % 10;
+			randomNum = ((rand() % 2) + 1) * 2;
+			cur_board[i][j] = randomNum;
 
 			system("cls");
-			for (i=0; i<size; i++) {
-				for (j=0; j<size; j++) {
-					if (j==0)
+			for (i = 0; i < size; i++) {
+				for (j = 0; j < size; j++) {
+					if (j == 0)
 						printf("|");
 					printf(" %4d   |", pre_board[i][j]);
 				}
@@ -140,87 +109,57 @@ int main() {
 			printf("\nThe previous state\n");
 			printf("Score:%d\nHigh Score:%d\n", pre_score, high_score);
 
-			for (i=0; i <size; i++) {
-				for (j=0; j<size; j++) {
-					if (j==0)
+			for (i = 0; i < size; i++) {
+				for (j = 0; j < size; j++) {
+					if (j == 0)
 						printf("|");
 					printf(" %4d   |", cur_board[i][j]);
 				}
 				printf("\n");
 			}
-			sum=0;
-			for (i=0; i <size; i++) {
-				for (j=0; j <size; j++) {
-					sum+=cur_board[i][j];
+			sum = 0;
+			for (i = 0; i < size; i++) {
+				for (j = 0; j < size; j++) {
+					sum += cur_board[i][j];
 				}
 			}
-			cur_score=sum;
+			cur_score = sum;
 			if (cur_score > high_score) {
-				high_score=cur_score;
+				high_score = cur_score;
 			}
 			printf("\nThe current state\n");
 			printf("Score:%d\nHigh Score:%d\n", cur_score, high_score);
 		}
-		if (key=='a') {
-			pre_score=cur_score;
+		if (key == 'a') {
+			pre_score = cur_score;
 
-			for (i=0; i<=size-1; i++) {
-				for (j=0; j<=size-1; j++) {
-					pre_board[i][j]=cur_board[i][j];
+			for (i = 0; i <= size - 1; i++) {
+				for (j = 0; j <= size - 1; j++) {
+					pre_board[i][j] = cur_board[i][j];
 				}
 			}
-			for (k=0; k<=size-2; k++) {
-				for (i=0; i<=size-1; i++) {
-					for (j=size-1; j > 0; j--) {
-						if (cur_board[i][j]!=0&&cur_board[i][j-1]==0) {
-							cur_board[i][j-1]=cur_board[i][j];
-							cur_board[i][j]=0;
-						}
-					}
-				}
-			}
-			for (i=0; i<=size-1; i++) {
-				j=0;
-				flag=0;
 
-				while (j<=size-2 && flag==0) {
-					if (cur_board[i][j]==cur_board[i][j+1]&&cur_board[i][j]!=0) {
-						cur_board[i][j]=cur_board[i][j+1]+cur_board[i][j];
-						cur_board[i][j+1]=0;
-						flag=1;
-					}
-					j=j+1;
-				}
-			}
-			for (k=0; k<=size-1; k++) {
-				for (i=0; i<=size-1; i++) {
-					for (j=size-1; j>0; j--) {
-						if (cur_board[i][j]!=0&&cur_board[i][j-1]==0) {
-							cur_board[i][j-1]=cur_board[i][j];
-							cur_board[i][j]=0;
-						}
+			move(key, cur_board, pre_board, size);
+
+			emptyTile = 0;
+			for (i = 0; i <= size - 1; i++) {
+				for (j = 0; j <= size - 1; j++) {
+					if (cur_board[i][j] == 0) {
+						emptyIndex[emptyTile] = 10 * i + j;
+						emptyTile = emptyTile + 1;
 					}
 				}
 			}
-			emptyTile=0;
-			for (i=0; i<=size-1; i++) {
-				for (j=0; j<=size-1; j++) {
-					if (cur_board[i][j]==0) {
-						emptyIndex[emptyTile]=10*i+j;
-						emptyTile=emptyTile+1;
-					}
-				}
-			}
-			randomIndex=rand()%emptyTile;
-			i=emptyIndex[randomIndex]/10;
-			j=emptyIndex[randomIndex]%10;
-			randomNum=((rand()%2)+1)*2;
-			cur_board[i][j]=randomNum;
+			randomIndex = rand() % emptyTile;
+			i = emptyIndex[randomIndex] / 10;
+			j = emptyIndex[randomIndex] % 10;
+			randomNum = ((rand() % 2) + 1) * 2;
+			cur_board[i][j] = randomNum;
 
 			system("CLS");
-			for (i=0; i <size; i++) {
-				for (j=0; j<size; j++) {
-					if (j==0)
+			for (i = 0; i < size; i++) {
+				for (j = 0; j < size; j++) {
+					if (j == 0)
 						printf("|");
 					printf(" %4d   |", pre_board[i][j]);
 				}
@@ -228,85 +167,56 @@ int main() {
 			}
 			printf("\nThe previous state\n");
 			printf("Score:%d\nHigh Score:%d\n", pre_score, high_score);
-			for (i=0; i <size; i++) {
-				for (j=0; j<size; j++) {
-					if (j==0)
+			for (i = 0; i < size; i++) {
+				for (j = 0; j < size; j++) {
+					if (j == 0)
 						printf("|");
 					printf(" %4d   |", cur_board[i][j]);
 				}
 				printf("\n");
 			}
-			sum=0;
-			for (i=0; i <size; i++) {
-				for (j=0; j <size; j++) {
-					sum+=cur_board[i][j];
+			sum = 0;
+			for (i = 0; i < size; i++) {
+				for (j = 0; j < size; j++) {
+					sum += cur_board[i][j];
 				}
 			}
-			cur_score=sum;
+			cur_score = sum;
 			if (cur_score > high_score) {
-				high_score=cur_score;
+				high_score = cur_score;
 			}
 			printf("\nThe current state\n");
 			printf("Score:%d\nHigh Score:%d", cur_score, high_score);
 		}
-		if (key=='s') {
-			pre_score=cur_score;
-			for (i=0; i<=size-1; i++) {
-				for (j=0; j<=size-1; j++) {
-					pre_board[i][j]=cur_board[i][j];
+		if (key == 's') {
+			pre_score = cur_score;
+			for (i = 0; i <= size - 1; i++) {
+				for (j = 0; j <= size - 1; j++) {
+					pre_board[i][j] = cur_board[i][j];
 				}
 			}
-			for (k=0; k<=size-1; k++) {
-				for (j=0; j<=size-1; j++) {
-					for (i=size-1; i>0; i--) {
-						if (cur_board[i][j]==0&&cur_board[i-1][j]!=0) {
-							cur_board[i][j]=cur_board[i-1][j];
-							cur_board[i-1][j]=0;
-						}
+
+			move(key, cur_board, pre_board, size);
+
+			emptyTile = 0;
+			for (i = 0; i <= size - 1; i++) {
+				for (j = 0; j <= size - 1; j++) {
+					if (cur_board[i][j] == 0) {
+						emptyIndex[emptyTile] = 10 * i + j;
+						emptyTile = emptyTile + 1;
 					}
 				}
 			}
-			for (j=0; j<=size-1; j++) {
-				i=size-1;
-				flag=0;
-				while (i>0 && flag==0) {
-					if (cur_board[i][j]==cur_board[i-1][j]&&cur_board[i][j]!=0) {
-						cur_board[i][j]=cur_board[i-1][j]+cur_board[i][j];
-						cur_board[i-1][j]=0;
-						flag=1;
-					}
-					i=i-1;
-				}
-			}
-			for (k=0; k<=size-1; k++) {
-				for (j=0; j<=size-1; j++) {
-					for (i=size-1; i>0; i--) {
-						if (cur_board[i][j]==0&&cur_board[i-1][j]!=0) {
-							cur_board[i][j]=cur_board[i-1][j];
-							cur_board[i-1][j]=0;
-						}
-					}
-				}
-			}
-			emptyTile=0;
-			for (i=0; i<=size-1; i++) {
-				for (j=0; j<=size-1; j++) {
-					if (cur_board[i][j]==0) {
-						emptyIndex[emptyTile]=10*i+j;
-						emptyTile=emptyTile+1;
-					}
-				}
-			}
-			randomIndex=rand()%emptyTile;
-			i=emptyIndex[randomIndex]/10;
-			j=emptyIndex[randomIndex]%10;
-			randomNum=((rand()%2)+1)*2;
-			cur_board[i][j]=randomNum;
+			randomIndex = rand() % emptyTile;
+			i = emptyIndex[randomIndex] / 10;
+			j = emptyIndex[randomIndex] % 10;
+			randomNum = ((rand() % 2) + 1) * 2;
+			cur_board[i][j] = randomNum;
 
 			system("CLS");
-			for (i=0; i<size; i++) {
-				for (j=0; j<size; j++) {
-					if (j==0)
+			for (i = 0; i < size; i++) {
+				for (j = 0; j < size; j++) {
+					if (j == 0)
 						printf("|");
 					printf(" %4d   |", pre_board[i][j]);
 				}
@@ -314,89 +224,59 @@ int main() {
 			}
 			printf("\nThe previous state\n");
 			printf("Score:%d\nHigh Score:%d\n", pre_score, high_score);
-			for (i=0; i<size; i++) {
-				for (j=0; j<size; j++) {
-					if (j==0)
+			for (i = 0; i < size; i++) {
+				for (j = 0; j < size; j++) {
+					if (j == 0)
 						printf("|");
 					printf(" %4d   |", cur_board[i][j]);
 				}
 				printf("\n");
 			}
 
-			sum=0;
-			for (i=0; i <size; i++) {
-				for (j=0; j <size; j++) {
-					sum+=cur_board[i][j];
+			sum = 0;
+			for (i = 0; i < size; i++) {
+				for (j = 0; j < size; j++) {
+					sum += cur_board[i][j];
 				}
 			}
 
-			cur_score=sum;
+			cur_score = sum;
 			if (cur_score > high_score) {
-				high_score=cur_score;
+				high_score = cur_score;
 			}
 			printf("\nThe current state\n");
 			printf("Score:%d\nHigh Score:%d", cur_score, high_score);
 		}
-		if (key=='w') {
-			pre_score=cur_score;
-			for (i=0; i<=size-1; i++) {
-				for (j=0; j<=size-1; j++) {
-					pre_board[i][j]=cur_board[i][j];
+		if (key == 'w') {
+			pre_score = cur_score;
+			for (i = 0; i <= size - 1; i++) {
+				for (j = 0; j <= size - 1; j++) {
+					pre_board[i][j] = cur_board[i][j];
 				}
 			}
-			for (k=0; k<=size-2; k++) {
-				for (j=0; j<=size-1; j++) {
-					for (i=0; i<size-1; i++) {
-						if (cur_board[i][j]==0&&cur_board[i+1][j]!=0) {
-							cur_board[i][j]=cur_board[i+1][j];
-							cur_board[i+1][j]=0;
-						}
-					}
-				}
-			}
-			for (j=0; j<=size-1; j++) {
-				i=0;
-				flag=0;
-				while (i<size-1 && flag==0) {
 
-					if (cur_board[i][j]==cur_board[i+1][j]&&cur_board[i][j]!=0) {
-						cur_board[i][j]=cur_board[i+1][j]+cur_board[i][j];
-						cur_board[i+1][j]=0;
-						flag=1;
-					}
-					i=i+1;
-				}
-			}
-			for (k=0; k<=size-2; k++) {
-				for (j=0; j<=size-1; j++) {
-					for (i=0; i <size-1; i++) {
-						if (cur_board[i][j]==0&&cur_board[i+1][j]!=0) {
-							cur_board[i][j]=cur_board[i+1][j];
-							cur_board[i+1][j]=0;
-						}
-					}
-				}
-			}
-			emptyTile=0;
-			for (i=0; i<=size-1; i++) {
-				for (j=0; j<=size-1; j++) {
-					if (cur_board[i][j]==0) {
-						emptyIndex[emptyTile]=10*i+j;
-						emptyTile=emptyTile+1;
+			move(key, cur_board, pre_board, size);
+
+			emptyTile = 0;
+			for (i = 0; i <= size - 1; i++) {
+				for (j = 0; j <= size - 1; j++) {
+					if (cur_board[i][j] == 0) {
+						emptyIndex[emptyTile] = 10 * i + j;
+						emptyTile = emptyTile + 1;
 					}
 				}
 			}
 
-			randomIndex=rand()%emptyTile;
-			i=emptyIndex[randomIndex]/10;
-			j=emptyIndex[randomIndex]%10;
-			randomNum=((rand()%2)+1)*2;
-			cur_board[i][j]=randomNum;
+			randomIndex = rand() % emptyTile;
+			i = emptyIndex[randomIndex] / 10;
+			j = emptyIndex[randomIndex] % 10;
+			randomNum = ((rand() % 2) + 1) * 2;
+			cur_board[i][j] = randomNum;
 
 			system("CLS");
-			for (i=0; i<size; i++) {
-				for (j=0; j<size; j++) {
-					if (j==0)
+			for (i = 0; i < size; i++) {
+				for (j = 0; j < size; j++) {
+					if (j == 0)
 						printf("|");
 					printf(" %4d   |", pre_board[i][j]);
 				}
@@ -404,38 +284,38 @@ int main() {
 			}
 			printf("\nThe previous state\n");
 			printf("Score:%d\nHigh Score:%d\n", pre_score, high_score);
-			for (i=0; i<size; i++) {
-				for (j=0; j<size; j++) {
-					if (j==0)
+			for (i = 0; i < size; i++) {
+				for (j = 0; j < size; j++) {
+					if (j == 0)
 						printf("|");
 					printf(" %4d   |", cur_board[i][j]);
 				}
 				printf("\n");
 			}
-			sum=0;
-			for (i=0; i<size; i++) {
-				for (j=0; j<size; j++) {
-					sum+=cur_board[i][j];
+			sum = 0;
+			for (i = 0; i < size; i++) {
+				for (j = 0; j < size; j++) {
+					sum += cur_board[i][j];
 				}
 			}
-			cur_score=sum;
+			cur_score = sum;
 			if (cur_score > high_score) {
-				high_score=cur_score;
+				high_score = cur_score;
 			}
 			printf("\nThe current state\n");
 			printf("Score:%d\nHigh Score=%d", cur_score, high_score);
 		}
-		if (key=='x') {
-			cur_score=0;
-			for (i=0; i<=size-1; i++) {
-				for (j=0; j<=size-1; j++) {
-					cur_board[i][j]=0;
+		if (key == 'x') {
+			cur_score = 0;
+			for (i = 0; i <= size - 1; i++) {
+				for (j = 0; j <= size - 1; j++) {
+					cur_board[i][j] = 0;
 				}
 			}
 			system("CLS");
-			for (i=0; i<size; i++) {
-				for (j=0; j<size; j++) {
-					if (j==0)
+			for (i = 0; i < size; i++) {
+				for (j = 0; j < size; j++) {
+					if (j == 0)
 						printf("|");
 					printf(" %4d   |", pre_board[i][j]);
 				}
@@ -443,9 +323,9 @@ int main() {
 			}
 			printf("\nThe previous state\n");
 			printf("Score:%d\nHigh Score:%d\n", pre_score, high_score);
-			for (i=0; i<size; i++) {
-				for (j=0; j<size; j++) {
-					if (j==0)
+			for (i = 0; i < size; i++) {
+				for (j = 0; j < size; j++) {
+					if (j == 0)
 						printf("|");
 					printf(" %4d   |", cur_board[i][j]);
 				}
@@ -454,11 +334,11 @@ int main() {
 			printf("\nThe current state\n");
 			printf("Score:%d\nHigh Score:%d", cur_score, high_score);
 		}
-		if (key=='r') {
+		if (key == 'r') {
 			system("CLS");
-			for (i=0; i<size; i++) {
-				for (j=0; j<size; j++) {
-					if (j==0)
+			for (i = 0; i < size; i++) {
+				for (j = 0; j < size; j++) {
+					if (j == 0)
 						printf("|");
 					printf(" %4d   |", cur_board[i][j]);
 				}
@@ -466,9 +346,9 @@ int main() {
 			}
 			printf("\nThe previous state\n");
 			printf("Score:%d\nHigh Score:%d\n", cur_score, high_score);
-			for (i=0; i<size; i++) {
-				for (j=0; j<size; j++) {
-					if (j==0)
+			for (i = 0; i < size; i++) {
+				for (j = 0; j < size; j++) {
+					if (j == 0)
 						printf("|");
 					printf(" %4d   |", pre_board[i][j]);
 				}
@@ -476,65 +356,65 @@ int main() {
 			}
 			printf("\nThe current state\n");
 			printf("Score:%d\nHigh Score:%d", pre_score, high_score);
-			for (i=0; i<size; i++) {
-				for (j=0; j<size; j++) {
-					cur_board[i][j]=pre_board[i][j];
+			for (i = 0; i < size; i++) {
+				for (j = 0; j < size; j++) {
+					cur_board[i][j] = pre_board[i][j];
 				}
 			}
 		}
-		isGameover=0;
-		for (i=0; i<=size-1; i++) {
-			for (j=0; j<=size-1; j++) {
-				if (cur_board[i][j]==0) {
-					isGameover=1;
+		isGameover = 0;
+		for (i = 0; i <= size - 1; i++) {
+			for (j = 0; j <= size - 1; j++) {
+				if (cur_board[i][j] == 0) {
+					isGameover = 1;
 				}
 			}
 		}
-		for (i=0; i<=size-2; i++) {
-			for (j=0; j<=size-2; j++) {
-				if (i==size-1 && j!=size-1 && cur_board[i][j]==cur_board[i][j+1]) {
-					isGameover=1;
+		for (i = 0; i <= size - 2; i++) {
+			for (j = 0; j <= size - 2; j++) {
+				if (i == size - 1 && j != size - 1 && cur_board[i][j] == cur_board[i][j + 1]) {
+					isGameover = 1;
 				}
-				if (j==size-1 && i!=size-1 && cur_board[i][j]==cur_board[i+1][j]) {
-					isGameover=1;
+				if (j == size - 1 && i != size - 1 && cur_board[i][j] == cur_board[i + 1][j]) {
+					isGameover = 1;
 				}
-				if (cur_board[i][j]==cur_board[i+1][j] || cur_board[i][j]==cur_board[i][j+1]) {
-					isGameover=1;
+				if (cur_board[i][j] == cur_board[i + 1][j] || cur_board[i][j] == cur_board[i][j + 1]) {
+					isGameover = 1;
 				}
 			}
 		}
-		if (isGameover!=1) {
-			key='e';
+		if (isGameover != 1) {
+			key = 'e';
 		}
-		i=1;
-		j=1;
-		isWin=0;
-		while (i<size && isWin==0) {
-			while (j<size && isWin==0) {
-				if (cur_board[i][j]==2048) {
-					isWin=1;
+		i = 1;
+		j = 1;
+		isWin = 0;
+		while (i < size && isWin == 0) {
+			while (j < size && isWin == 0) {
+				if (cur_board[i][j] == 2048) {
+					isWin = 1;
 				}
-				j+=1;
+				j += 1;
 			}
-			i+=1;
+			i += 1;
 		}
 		printf("\nPress 8 to move up,press 4 to move left,press move 2 to down,press 6 to move right,\npress e to finish the game,press x to refresh,press r to restore \n");
-		if (isWin==1) {
+		if (isWin == 1) {
 			printf("YOU WON!\n");
-			key='e';
+			key = 'e';
 		}
 	}
 	system("CLS");
-	for (i=0; i<size; i++) {
-		for (j=0; j<size; j++) {
-			if (j==0)
+	for (i = 0; i < size; i++) {
+		for (j = 0; j < size; j++) {
+			if (j == 0)
 				printf("|");
 			printf(" %4d   |", cur_board[i][j]);
 		}
 		printf("\n");
 	}
 	printf("\nThe current state\n");
-	if (isWin!=1) {
+	if (isWin != 1) {
 		printf("\nYOU LOSE!");
 	}
 	printf("\n \n GAME OVER!\n Score: %d \n High Score:%d", cur_score, high_score);
